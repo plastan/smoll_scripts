1492b30c452e00434462fe4a26c6289d8e8ae654
plastan <angelofthousandeyes@gmail.com>
2023-09-28 18:11:31 +0530

    rewrite

---
+++ h/h2/main.c
@@ -0,0 +1,3 @@
+The touch command in Linux is used to create empty files, but it doesn't create directories if they don't exist. If you want to create a file inside a non-existing directory, you'll need to create the directory first. You can use the mkdir command to create the directory and then use touch to create the file within it.
+
+include <stdio.h>
